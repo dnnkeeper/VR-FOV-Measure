9a380e61e8a8a9761a69d5b09440ba408be12fd3
dnnkeeper <dnnkeeper@gmail.com>
2020-07-07 13:24:46 +0300

    Initial release

---
+++ Assets/FOV Test/Scripts/ControllerAngle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerAngle : MonoBehaviour
+{
+    public FOVMeasure fovMeasure;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(fovMeasure.transform.position, fovMeasure.transform.forward, Color.red);
+        Debug.DrawRay(fovMeasure.transform.position, transform.position - fovMeasure.transform.position, Color.yellow);
+
+        fovMeasure.FOV = 2f * Vector3.Angle(fovMeasure.transform.forward, transform.position - fovMeasure.transform.position);
+    }
+}
---
+++ Assets/FOV Test/Scripts/FOVMeasure.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FOVMeasure : MonoBehaviour
+{
+    public float FOV = 60;
+
+    public float circleDistance = 10f;
+
+    public LineRenderer lineRenderer;
+
+    public TextMeshProUGUI text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 360; i++)
+        {
+            circlePoints.Add(Vector3.zero);
+        }
+
+        if (lineRenderer == null)
+            lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 360;
+
+    }
+
+    List<Vector3> circlePoints = new List<Vector3>(360);
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 circleCenter = transform.position + (transform.forward * circleDistance);
+        //Debug.DrawLine(transform.position, circleCenter, Color.red);
+
+        for (int i = 0; i < 360; i++)
+        {
+            if (circlePoints[i] != null)
+            {
+                circlePoints[i] = transform.position + transform.rotation * Quaternion.Euler(0, 0, i) * Quaternion.Euler(0f, FOV / 2f, 0f) * (Vector3.forward * circleDistance);
+                //Debug.DrawLine(circleCenter, circlePoints[i].transform.position, Color.blue);
+            }
+        }
+        lineRenderer.SetPositions(circlePoints.ToArray());
+
+        if (text != null)
+        {
+            text.text = FOV.ToString("F1") + "°";
+        }
+    }
+}
